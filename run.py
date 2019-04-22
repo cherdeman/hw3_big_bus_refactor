7378bc86ba4f888d3cc60f235d11be7fd011d63f
Claire Herdeman <cherdeman@uchicago.edu>
2019-04-22 11:15:40 -0500

    putting everyting in run into main function

--- run.py
+++ run.py
@@ -2,25 +2,25 @@ from datetime import datetime, timedelta
 from routes import Route, Seller
 from control import Control
 
-# Route dictionary: key = route color, val = num buses on route
-rdict = {'blue': 2, 'green': 4, 'red': 5}
-
-# Initialize date dict and get current date
-ddict = {}
-now = datetime.today()
-
-# Associate appropriate route info with each date for next 10 days
-for i in range(10):
-    d = now+timedelta(days = i)
-    d = d.strftime("%B/%d/%Y")
-    ddict[d] = {}
-    for color, num in rdict.items():
-        ddict[d][color] = Route(color, num)
-
-# Initialize seller
-s = Seller(ddict)
 
 def main():
+	# Route dictionary: key = route color, val = num buses on route
+	rdict = {'blue': 2, 'green': 4, 'red': 5}
+
+	# Initialize date dict and get current date
+	ddict = {}
+	now = datetime.today()
+
+	# Associate appropriate route info with each date for next 10 days
+	for i in range(10):
+	    d = now+timedelta(days = i)
+	    d = d.strftime("%B/%d/%Y")
+	    ddict[d] = {}
+	    for color, num in rdict.items():
+	        ddict[d][color] = Route(color, num)
+
+	# Initialize seller
+	s = Seller(ddict)
 	c = Control(s)
 	c.control()
 
