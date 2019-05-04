88139a81e28eda7c4723c5f4ea69d23b71c1a1e0
Claire Herdeman <cherdeman@uchicago.edu>
2019-05-04 18:19:42 -0500

    red

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -19,7 +19,13 @@ def test_total_seats():
 def test_tickets_sold():
     tr.tickets_sold == {}
 
+# test route methods 
 def test_sell_ticket():
     ticket = ('12345', 'blue', 'May 5, 2018', 2)
     tr.sell_ticket(ticket)
     tr.tickets_sold['12345'] == ticket
+
+def test_refund_ticket():
+    ticket_id = '12345'
+    tr.refund_ticket('12345')
+    tr.tickets_solid == {}
