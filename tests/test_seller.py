917f0802cfddcce833caa3becfb78d7df3e71b94
Claire Herdeman <cherdeman@uchicago.edu>
2019-05-04 18:46:27 -0500

    red

--- tests/test_seller.py
+++ tests/test_seller.py
@@ -1,3 +1,19 @@
 import pytest
+from datetime import datetime, timedelta
 from classes.routes import Route 
 from classes.seller import Seller
+
+
+#initialize test seller
+now = datetime(2019, 5, 4, 18, 39, 53, 898306)
+date_dict = {}
+for i in range(10):
+    date = now + timedelta(days = i)
+    date = date.strftime("%m/%d/%Y")
+    date_dict[date] = {}
+    date_dict[date]['blue'] = Route('blue', 5)
+
+ts = Seller(date_dict)
+
+def test_check_input_date():
+	ts.check_input_date('05/04/2019') == True
