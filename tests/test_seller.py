006651fc6a85c24b631d880b3fa199f84cb32bda
Claire Herdeman <cherdeman@uchicago.edu>
2019-05-05 11:11:52 -0500

    green

--- tests/test_seller.py
+++ tests/test_seller.py
@@ -91,6 +91,12 @@ def test_confirm_ticket_sale():
     ts.get_route()
     ts.confirm_order() == True
 
+@patch('builtins.input', lambda _ : 'n')
+def test_confirm_ticket_sale_reject():
+    ts.date = '05/10/2019'
+    ts.get_route()
+    ts.confirm_order() == False
+
 @patch('builtins.input', lambda _ : 'blue')
 def test_print_report_today_correct():
     ts.date == '05/04/2019'
