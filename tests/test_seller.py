b59dd7662885f371ee6f678cd23497db0c6a5e9e
Claire Herdeman <cherdeman@uchicago.edu>
2019-05-04 21:02:14 -0500

    red

--- tests/test_seller.py
+++ tests/test_seller.py
@@ -18,29 +18,35 @@ ts = Seller(date_dict, 1.2, 0.9, 10)
 
 # test attributes
 def test_available_dates():
-	ts.routes_by_date == date_dict
+    ts.routes_by_date == date_dict
 
 def test_weekend_multiplier():
-	ts.weekend_multiplier == 1.2
+    ts.weekend_multiplier == 1.2
 
 def test_group_discount_rate():
-	ts.group_discount_rate == 0.9
+    ts.group_discount_rate == 0.9
 
 def test_base_ticket_price():
-	ts.base_ticket_price == 10
+    ts.base_ticket_price == 10
 
 # test methods
 def test_check_input_date():
-	ts.check_input_date('05/04/2019') == True
+    ts.check_input_date('05/04/2019') == True
 
 def test_get_price_weekend():
-	ts.get_price('05/04/2019') == 12
+    ts.get_price('05/04/2019') == 12
 
 def test_get_price_weekday():
-	ts.get_price('05/09/2019') == 10
+    ts.get_price('05/09/2019') == 10
 
 def test_get_route():
-	ts.get_route('05/04/2019', 'blue').color == 'blue'
+    ts.get_route('05/04/2019', 'blue').color == 'blue'
 
-def test_check_seat_availability():
-	ts.check_seat_availability(5) == False
+def test_check_ticket_limit_4():
+    ts.check_ticket_limit(4) == True
+
+def test_check_ticket_limit_5():
+    ts.check_ticket_limit(5) == False
+
+# def test_check_seat_availability():
+#     ts.check_seat_availability(5) == True
