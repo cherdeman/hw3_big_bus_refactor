7992ddd3ea4957fcb9df71d9fb2bdb3d41ae3733
Claire Herdeman <cherdeman@uchicago.edu>
2019-05-04 20:15:15 -0500

    red

--- tests/test_seller.py
+++ tests/test_seller.py
@@ -26,7 +26,11 @@ def test_weekend_multiplier():
 def test_group_discount_rate():
 	ts.group_discount_rate == 0.9
 
-
 # test methods
 def test_check_input_date():
 	ts.check_input_date('05/04/2019') == True
+
+def test_get_price():
+	ts.get_price('05/04/2019') == 12
+	#route = ts.routes_by_date['05/04/2019'][blue]
+	#route.base_ticket_price
