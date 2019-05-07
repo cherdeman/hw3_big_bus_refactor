5d4b746ed96ac3f2f5a14a07fa1346b197c5f407
Claire Herdeman <cherdeman@uchicago.edu>
2019-05-06 21:48:41 -0500

    refactor

--- classes/control.py
+++ classes/control.py
@@ -4,10 +4,10 @@ class Control():
     """Class to provide CLI for interaction with the system"""
     def __init__(self, seller):
         self.seller = seller
-        self.options = {1: "Sell tickets",
-                        2: "Provide a refund",
-                        3: "Generate a report",
-                        4: "Exit"}
+        self.options = {1: ("Sell tickets", lambda: self.seller.sell_ticket()),
+                        2: ("Provide a refund", lambda: self.seller.refund()),
+                        3: ("Generate a report", lambda: self.seller.report()),
+                        4: ("Exit", lambda: self.set_active())}
         self.active = True
 
     def control(self):
@@ -16,19 +16,22 @@ class Control():
             print()
             print("What would you like to do?")
             for i in range(1, len(self.options) + 1):
-                print(f"{i}) {self.options[i]}".format())
+                print(f"{i}) {self.options[i][0]}")
 
             option = self.get_option()
 
-            if option == 1:
-                self.seller.sell_ticket()
-            elif option == 2:
-                self.seller.refund()
-            elif option == 3:
-                self.seller.report()
-            elif option == 4:
-                print("Bye")
-                self.active = False
+            if option in self.options.keys():
+                action_tuple = self.options[option]
+                action_tuple[1]()
+            # if option == 1:
+            #     self.seller.sell_ticket()
+            # elif option == 2:
+            #     self.seller.refund()
+            # elif option == 3:
+            #     self.seller.report()
+            # elif option == 4:
+            #     print("Bye")
+            #     self.active = False
             else: 
                 print("That is not valid input.")
 
@@ -40,4 +43,10 @@ class Control():
             option = None
 
         return option
+
+    def set_active(self):
+        if self.active is True:
+            self.active = False
+        else:
+            self.active = True
 
